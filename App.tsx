c9f31701e3239b2822d05fa95e6aafe523d09515
luanrichardsz <luanrchardof@gmail.com>
2024-09-17 21:09:49 -0300

    100% atualizado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { View } from 'react-native';
+import {
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    LocationObject,
+    watchPositionAsync,
+    LocationAccuracy
+} from 'expo-location';
+
+
+import { styles } from './style';
 
 export default function App() {
+  const  [location, setLocation] = useState<LocationObject | null>(null);
+
+  async function requetsLocationPermissions(){
+    const {granted} = await requestForegroundPermissionsAsync();
+
+    if(granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requetsLocationPermissions();
+  },[]);
+
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1
+    }, (response) =>{
+
+    })
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    {
+      location && 
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      > 
+       <Marker 
+       //MARCADOR
+       coordinate={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+       }}
+       />
+      </MapView>
+    }
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
